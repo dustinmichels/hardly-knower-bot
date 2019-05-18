9eb8be67204ce0c13fab4e61788115c127bc3315
Dustin Michels <dustin7538@gmail.com>
2019-05-18 01:33:55 -0600

    different style, one word at a time

--- GAS/Code.ts
+++ GAS/Code.ts
@@ -5,7 +5,6 @@ const keywords = [
   "incur",
   "blender",
   "liqueur",
-  "concur",
   "prefer",
   "recur",
   "confer",
@@ -22,11 +21,17 @@ const keywords = [
 
 function main() {
   initializeOAuthClient();
-  search();
+
+  for (let word of keywords) {
+    Logger.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    Logger.log(word);
+    search(word);
+  }
 }
 
-function search() {
-  let tweets = getTweets();
+function search(word: string) {
+  let tweets = getTweets(word);
+
   for (let tweet of tweets) {
     // get text
     let text = <string>tweet.text;
@@ -34,7 +39,7 @@ function search() {
 
     // get final words
     let finalWord = clean(arr.pop());
-    let penultimateWord = clean(arr.pop());
+    // let penultimateWord = clean(arr.pop());
 
     // Logger.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     // Logger.log(finalWord);
@@ -42,15 +47,25 @@ function search() {
 
     // check if final word in keywords list
     if (keywords.indexOf(finalWord) >= 0) {
-      // Logger.log("==== final word match! ====");
       Logger.log(text);
       Logger.log(`>> ${finalWord}?? I hardly know 'er!`);
-    } else if (keywords.indexOf(penultimateWord) >= 0) {
-      // Logger.log("==== penultimate word match! ====");
-      Logger.log(text);
-      Logger.log(`>> ${penultimateWord}?? I hardly know 'er!`);
     }
+    // if (keywords.indexOf(penultimateWord) >= 0) {
+    // Logger.log("==== penultimate word match! ====");
+    // Logger.log(text);
+    // Logger.log(`>> ${penultimateWord}?? I hardly know 'er!`);
+    // }
   }
+}
+
+function getTweets(word) {
+  const search_url = "https://api.twitter.com/1.1/search/tweets.json";
+  const params = `?q=${word}&lang=en&count=50`;
+  var url = search_url + params;
+
+  var response = authUrlFetch.fetch(url);
+  let json = JSON.parse(response.getContentText());
+  return json["statuses"];
 }
 
 /** Standardize word, remove puncuation & emoji characters */
@@ -63,15 +78,4 @@ function clean(word: string) {
       /([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])/g,
       ""
     );
-}
-
-function getTweets() {
-  const search_url = "https://api.twitter.com/1.1/search/tweets.json";
-  const keywordParams = keywords.join("+OR+");
-  const params = `?q=${keywordParams}&lang=en&count=50`;
-  var url = search_url + params;
-
-  var response = authUrlFetch.fetch(url);
-  let json = JSON.parse(response.getContentText());
-  return json["statuses"];
 }
